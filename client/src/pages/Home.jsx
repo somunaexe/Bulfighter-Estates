7293070b5b4fa4ee7543a400cb9a106ad4557012
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2024-10-17 00:25:51 +0100

    added listing type section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Header from "../components/Header"
 import Hero from "../components/Hero"
 import Features from "../components/Features"
+import Listings from "../components/Listings"
+import About from "../components/About"
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
         <Header />
         <Hero />
         <Features />
+        <Listings />
+        <About />
     </>
   )
 }
