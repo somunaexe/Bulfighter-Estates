d394193dd265cf03765208ae6c3ffc85751cf0da
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2024-10-16 22:27:15 +0100

    header has been set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,13 @@ import Login from "./pages/Login"
 export default function App() {
   return (
     <BrowserRouter>
+    <div className="text-[#404040] bg-primary">
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
       </Routes>
+    </div>
     </BrowserRouter>
   )
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,22 +1,44 @@
-import React from 'react'
-import { Link } from "react-router-dom"
+import React, { useState } from 'react'
+import { Link, useNavigate } from "react-router-dom"
 import { FaSearch, FaUser } from "react-icons/fa"
 
 const Header = () => {
+
+  const [menuOpened, setMenuOpened] = useState(false)
+  const [dropdownMenu, setDropdownMenu] = useState(false)
+  const navigate = useNavigate()
+
+  const toggleMenu = () => {
+    setMenuOpened(!menuOpened)
+  }
+
+  const toggleDropdown = () => {
+    setDropdownMenu(!dropdownMenu)
+  }
+
+
   return (
-    <header>
+    <header className='max-padd-container flexBetween rounded-xl py-4'>
       {/* {LOGO} */}
       <Link to={'/'} className='bold-24'>
-      <div>
-        Bulfighter  <span>Estates</span>
-      </div>
+        <div>
+          Bulfighter <span className='text-secondary'>Estates</span>
+        </div>
       </Link>
       {/* Searchable */}
-      <div>
-        <input type="text"
-        placeholder='Search here...'
+      <div className='bg-white ring-1 ring-slate-900/5 rounded-full p-2 px-4 w-44 sm:w-96 flexBetween gap-x-2 relative'>
+        <input type="text" placeholder='Search here...'
+        className='outline-none border-none w-full bg-white'
         />
-        <button><FaSearch /></button>
+        <button className='absolute right-0 h-full w-10 rounded-full bg-secondary text-white flexCenter cursor-pointer'>
+          <FaSearch />
+        </button>
+      </div>
+      {/* Dropdown Menu */}
+      <div>
+        <div>
+          <div><FaUser /></div>
+        </div>
       </div>
     </header>
   )
