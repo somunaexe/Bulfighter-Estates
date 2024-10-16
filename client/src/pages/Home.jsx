538e8e5554a6f79b16beac5e2a14f12b9ad96a98
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2024-10-16 23:42:39 +0100

    completed home page styling

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from "../components/Header"
+import Hero from "../components/Hero"
+import Features from "../components/Features"
 
 const Home = () => {
   return (
     <>
         <Header />
+        <Hero />
+        <Features />
     </>
   )
 }
