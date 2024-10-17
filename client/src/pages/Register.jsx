158029b4e4b7e3dd9dc6f5c6647b6ef91071a2fc
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2024-10-17 02:16:46 +0100

    register frontend has been set up

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,97 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from "react-router-dom"
+import { MdUpload } from "react-icons/md"
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    profileImage: null,
+  })
+
+  const handleChange = (e) => {
+    const {name, value, files} = e.target
+
+    setFormData({
+      ...formData,
+      [name]: value,
+      [name]: name === "profileImage" ? files[0] : value
+    })
+  }
+  console.log(formData)
   return (
-    <div>
-      
+    <div className='absolute h-full w-full bg-black/40 z-50 flexCenter'>
+      <div>
+        <form 
+          action="" 
+          className='flex flex-col gap-y-2.5 bg-white w-[366px] p-7 rounded-xl shadow-md text-[14px]'
+        >
+          <h4 className='h3 my-4'>Sign Up</h4>
+          <input 
+            onChange={handleChange}
+            value={formData.firstName}
+            type="text" 
+            name="firstName" 
+            placeholder='First Name' 
+            required
+            className='bg-primary border-none p-2 pl-4 rounded-md outline-none'
+          />
+          <input 
+            onChange={handleChange}
+            value={formData.lastName}
+            type="text" 
+            name='lastName' 
+            placeholder='Last Name' 
+            required
+            className='bg-primary border-none p-2 pl-4 rounded-md outline-none'
+          />
+          <input 
+            onChange={handleChange}
+            value={formData.email}
+            type="email" 
+            name='email' 
+            placeholder='Email Address' 
+            required
+            className='bg-primary border-none p-2 pl-4 rounded-md outline-none'
+          />
+          <input 
+            onChange={handleChange}
+            value={formData.password}
+            type="password" 
+            name='password' 
+            placeholder='Password' 
+            required
+            className='bg-primary border-none p-2 pl-4 rounded-md outline-none'
+          />
+          <input 
+            onChange={handleChange}
+            value={formData.confirmPassword}
+            type="text" 
+            name='confirmPassword' 
+            placeholder='Confirm Password' 
+            required
+            className='bg-primary border-none p-2 pl-4 rounded-md outline-none'
+          />
+          <input type="file" id='image' name="profileImage" accept="image/*" hidden required/>
+          <label htmlFor="image">
+            <div className='flexCenter ring-1 ring-slate-900/10 p-1 h-16 w-16 rounded'>
+            {formData.profileImage ? (
+              <img src={URL.createObjectURL(formData.profileImage)} alt="profileImg" className='p-1 h-16 object-contain aspect-square'/>
+            ) : (
+              <MdUpload className="text-tertiary text-2xl" />
+            )}
+            </div>
+          </label>
+          <button type='submit' className='btn-secondary rounded mt-2'>Register</button>
+          <div className='text-gray-30'>
+            Already have an account?
+            <Link to={'/login'} className='text-secondary cursor-pointer'>Login</Link>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
