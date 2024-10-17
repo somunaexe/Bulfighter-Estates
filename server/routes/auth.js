11d9749cb64155a5c3b6deac3fb51d774d22da1c
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2024-10-17 17:08:20 +0100

    login logic implemented

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -59,4 +59,30 @@ router.post("/register", upload.single("profileImage"), async (req,res)=>{
     }
 })
 
+//User Login
+router.post("/login", async (req, res) => {
+    try {
+        const {email, password} = req.body
+
+        //Check if User Exists
+        const user = await user.findOne({email})
+        if(!user){
+            return res.status(400).json({message: "User doesn't exist!"})
+        }
+
+        //Compare the password with hashedPassword
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch){
+            return res.status(400).json({message: "Invalid credentials"})
+        }
+
+        const token = jwt.sign({id:user._id}, process.env.JWT_SECRET)
+        delete user.password
+
+        res.status(200).json({token, user})
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({error:err.message})
+    }
+})
 export default router
